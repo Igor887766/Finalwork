db89ee117673e5a7619d648d82f2eb686f3f1c8e
Igor <887766@tut.by>
2022-12-13 22:27:07 +0300

    добавил коментарии к решению

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿void MyPrintArray(string[] localArray)  
+﻿void MyPrintArray(string[] localArray)  // метод для вывода массивов
 {
     Console.Write("[");
     for (int i =0; i < localArray.Length; i++)
@@ -12,7 +12,7 @@
 Console.Clear();
 Console.WriteLine("Добрый вечер! Будем переварачивать массив символов длина которых меньше, либо равна 3 символам");
 Console.Write("Введите количество строк исходного массива n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine()); // запрашиваем кол-во элементов массива
 
 string [] iArray = new string [n];  
 
@@ -20,7 +20,7 @@ int m = 0;
 Console.WriteLine();
 for (int i=0; i<iArray.Length; i++)  
 {
-    Console.Write($"Введите {i+1}-ю строку: ");
+    Console.Write($"Введите {i+1}-ю строку: "); // создаем массив
     iArray[i] = Console.ReadLine();
     if (iArray[i].Length<4)
     {
@@ -29,7 +29,7 @@ for (int i=0; i<iArray.Length; i++)
 }
 Console.WriteLine();
 Console.WriteLine("Исходный массив: ");
-MyPrintArray(iArray);
+MyPrintArray(iArray); // выводим исходный массив
 
 string [] rArray = new string [m];
 
@@ -37,12 +37,12 @@ int k = 0;
 
 for(int i=0; i<iArray.Length; i++)
 {
-    if (iArray[i].Length<4)
+    if (iArray[i].Length<4) // проверяем массив на кол-во символов меньше 4
     {
-        rArray[k] = iArray[i];
+        rArray[k] = iArray[i]; // создаем новый массив из символов меньше 4
         k++;
     }
 }
 Console.WriteLine();
 Console.WriteLine("Перевернутый массив: ");
-MyPrintArray(rArray);
+MyPrintArray(rArray); // выводим новый массив
